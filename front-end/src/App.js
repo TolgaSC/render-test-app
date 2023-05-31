59949df92a21f2dad2c1e9105390af5441d30b4a
Oliver Weber <ow@uix.io>
2023-05-31 11:43:42 +0200

    Anpassungen Lokal & Remote Environment

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
+  const [headline, setHeadline] = useState();
+  useEffect(() => {
+    fetch("http://localhost:3001/api/test")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setHeadline(data.headline);
+      })
+      .catch(console.error);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{headline}</h1>
       </header>
     </div>
   );
