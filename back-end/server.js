f7712774ee04db9243f1ca3392daac51135b09ac
TolgaSC <codingisthefuture21@gmail.com>
2023-05-31 16:46:38 +0200

    render-test-app

--- back-end/server.js
+++ back-end/server.js
@@ -12,15 +12,6 @@ app.get("/api/test", (req, res) => {
   res.send({ headline: "It works OMG" });
 });
 
-// NUR ZUR VERANSCHAULICHUNG
-// vereinfachte darstellung des CRA Proxies
-// alle requests gehen weiter auf Proxy (package.json[proxy])
-// response geht zurück als port :3000 anstatt :3001
-// Und dann ist der Browser (CORS) glücklich
-// app.all((req, res) => {
-//   fetch("http:localhost:3001" + req.url).then((data) => res.send(data));
-// });
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
